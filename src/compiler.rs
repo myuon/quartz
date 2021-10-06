465fc1b13b497916c018036651dbad1b0d6e6be6
myuon <ioi.joi.koi.loi@gmail.com>
2021-10-07 03:46:32 +0900

    fix: typecheck

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 
-use crate::{eval::Evaluator, parser::run_parser, vm::DataType};
+use crate::{eval::Evaluator, parser::run_parser, stdlib::typecheck_with_stdlib, vm::DataType};
 
 pub struct Compiler {}
 
@@ -10,7 +10,8 @@ impl Compiler {
     }
 
     pub fn exec(&self, input: &str) -> Result<DataType> {
-        let module = run_parser(input)?;
+        let mut module = run_parser(input)?;
+        typecheck_with_stdlib(&mut module)?;
 
         let mut eval = Evaluator::new();
         eval.eval_module(module)
