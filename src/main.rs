ab27343db3600de8374ab0fb0cd093921fb40c5b
myuon <ioi.joi.koi.loi@gmail.com>
2022-12-04 17:21:57 +0900

    feat: runner and the first test

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,22 @@ mod compiler;
 mod generator;
 mod lexer;
 mod parser;
+mod runtime;
 
-use wasmer::{imports, Instance, Module, Store, Value};
-
-use crate::compiler::Compiler;
+use crate::{compiler::Compiler, runtime::Runtime};
 
 fn main() -> anyhow::Result<()> {
     let mut compiler = Compiler::new();
-    let wat = compiler.compile(
+    let mut runtime = Runtime::new();
+    let wat = runtime.run(&compiler.compile(
         r#"
 fun main(): i32 {
   let x: i32 = 10;
   return x + 1;
 }
 "#,
-    )?;
-
-    let mut store = Store::default();
-    let module = Module::new(&store, &wat)?;
-    let import_object = imports! {};
-    let instance = Instance::new(&mut store, &module, &import_object)?;
-
-    let main = instance.exports.get_function("main")?;
-    let result = main.call(&mut store, &[])?;
-    assert_eq!(result[0], Value::I32(11));
+    )?)?;
+    println!("{:?}", wat);
 
     Ok(())
 }
---
+++ src/runtime.rs
@@ -0,0 +1,52 @@
+use anyhow::Result;
+use wasmer::{imports, Instance, Module, Store, Value};
+
+pub struct Runtime {}
+
+impl Runtime {
+    pub fn new() -> Runtime {
+        Runtime {}
+    }
+
+    pub fn run(&mut self, wat: &str) -> Result<Box<[Value]>> {
+        let mut store = Store::default();
+        let module = Module::new(&store, &wat)?;
+        let import_object = imports! {};
+        let instance = Instance::new(&mut store, &module, &import_object)?;
+
+        let main = instance.exports.get_function("main")?;
+        let result = main.call(&mut store, &[])?;
+
+        Ok(result)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+
+    use crate::compiler::Compiler;
+
+    use super::*;
+
+    #[test]
+    fn test_compile_and_run() {
+        let mut runtime = Runtime::new();
+        let cases = vec![(
+            r#"
+fun main(): i32 {
+    let x: i32 = 10;
+    return x + 1;
+}
+"#,
+            vec![Value::I32(11)],
+        )];
+
+        for (input, expected) in cases {
+            let mut compiler = Compiler::new();
+            let wat = compiler.compile(input).unwrap();
+            let result = runtime.run(&wat).unwrap();
+            assert_eq!(expected.as_slice(), result.as_ref());
+        }
+    }
+}
