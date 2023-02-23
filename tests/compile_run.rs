8ba947da9002f3d46111a3735f2c3950e0ea0d7f
myuon <ioi.joi.koi.loi@gmail.com>
2023-02-23 15:23:31 +0900

    test: create a process to run a compiler

---
+++ tests/compile_run.rs
@@ -0,0 +1,28 @@
+use std::io::Write;
+
+#[test]
+fn test_compile_run() {
+    let mut cmd = std::process::Command::new("cargo");
+    cmd.arg("run").arg("--").arg("run").arg("--stdin");
+
+    let mut child = cmd.stdin(std::process::Stdio::piped()).spawn().unwrap();
+    child
+        .stdin
+        .as_mut()
+        .unwrap()
+        .write_all(
+            b"
+fun main(): i32 {
+    let x: i32 = 10;
+    return x + 1;
+}
+",
+        )
+        .unwrap();
+
+    let output = child.wait_with_output().unwrap();
+    assert!(output.status.success());
+
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    assert_eq!(stdout, "11");
+}
