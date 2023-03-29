c7141a4386cbc1b208d1c488cdaef7dead2afb35
myuon <ioi.joi.koi.loi@gmail.com>
2023-03-29 22:34:31 +0900

    feat: support for need_line_follow

--- src/formatter.rs
+++ src/formatter.rs
@@ -164,7 +164,7 @@ impl<'s> Formatter<'s> {
         }
 
         let mut blocks = vec![];
-        let mut current_pos = 0; // for empty lines
+        let mut current_pos = 0;
         for (pos, line) in lines {
             let mut fwriter = Formatter::new(self.source, self.comments, self.comment_position);
             let mut buf = BufWriter::new(Vec::new());
@@ -172,13 +172,18 @@ impl<'s> Formatter<'s> {
             if self.need_empty_lines(current_pos, pos) {
                 fwriter.write_newline(&mut buf);
             }
-            current_pos = pos;
 
             fwriter.write(&mut buf, line.as_str());
 
-            blocks.push(String::from_utf8(buf.into_inner().unwrap()).unwrap());
+            if self.need_line_follow(current_pos, pos) {
+                let last_line = blocks.pop().unwrap();
+                blocks.push(format!("{} {}", last_line, line));
+            } else {
+                blocks.push(String::from_utf8(buf.into_inner().unwrap()).unwrap());
+            }
+
+            current_pos = pos;
         }
-        println!("{:?}", blocks);
 
         self.write(writer, "{");
         self.write_newline(writer);
@@ -493,6 +498,14 @@ impl<'s> Formatter<'s> {
             .filter(|p| p == &'\n')
             .count()
             >= 2
+    }
+
+    fn need_line_follow(&self, start: usize, end: usize) -> bool {
+        self.source[start..end]
+            .chars()
+            .filter(|p| p == &'\n')
+            .count()
+            == 0
     }
 
     fn consume_comments(&mut self, start: usize) -> Vec<Token> {
