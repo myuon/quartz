17772ec2732c65dfc01ee37e10ffa73d1986c977
myuon <ioi.joi.koi.loi@gmail.com>
2023-05-07 13:19:55 +0900

    fix: avoid infinite loop in replace

--- src/generator.rs
+++ src/generator.rs
@@ -122,7 +122,7 @@ impl Generator {
         })?;
 
         self.writer.start();
-        self.writer.write(r#"memory 50000"#);
+        self.writer.write(r#"memory 100"#);
         self.writer.end();
 
         for term in elements {
