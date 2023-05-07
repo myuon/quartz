03fb3d10d015fab5fad85a60e98fff16be5fd99a
myuon <ioi.joi.koi.loi@gmail.com>
2023-05-07 22:54:36 +0900

    feat: code gen for for-loop

--- src/generator.rs
+++ src/generator.rs
@@ -128,7 +128,7 @@ impl Generator {
         })?;
 
         self.writer.start();
-        self.writer.write(r#"memory 10000"#);
+        self.writer.write(r#"memory 50000"#);
         self.writer.end();
 
         for term in elements {
