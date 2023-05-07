02f5f719695db50a50f1eb0ebd8c899453069935
myuon <ioi.joi.koi.loi@gmail.com>
2023-05-07 21:24:59 +0900

    fix: type for make

--- src/generator.rs
+++ src/generator.rs
@@ -128,7 +128,7 @@ impl Generator {
         })?;
 
         self.writer.start();
-        self.writer.write(r#"memory 1000"#);
+        self.writer.write(r#"memory 10000"#);
         self.writer.end();
 
         for term in elements {
