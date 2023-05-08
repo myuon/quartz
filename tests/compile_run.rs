09b9f2b59b9d9494067a0449a74222ac7102e596
myuon <ioi.joi.koi.loi@gmail.com>
2023-05-09 01:34:54 +0900

    fix: t_enum

--- tests/compile_run.rs
+++ tests/compile_run.rs
@@ -291,6 +291,19 @@ fun main(): i32 {
     return p.at(40) + p.at(60);
 }
 "#,
+        r#"
+enum Value {
+    t_i32: i32,
+    t_string: string,
+}
+
+fun main(): i32 {
+    let t_10 = Value { t_i32: 10 };
+    let t_hello = Value { t_string: "hello" };
+
+    return t_10.t_i32! + t_hello.t_string!.length;
+}
+"#,
     ];
 
     for input in cases {
