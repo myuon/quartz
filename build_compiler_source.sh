8b892dad649656d3c4eecf5aa64c18908440eeb4
myuon <ioi.joi.koi.loi@gmail.com>
2023-05-27 14:54:53 +0900

    ci: fix

--- build_compiler_source.sh
+++ build_compiler_source.sh
@@ -1,4 +1,4 @@
-rm build/compiler/compiler.qz ; for file in ./quartz/*.qz; do
+mkdir -p build/compiler; rm build/compiler/compiler.qz; for file in ./quartz/*.qz; do
   if [ "$file" != "./quartz/std.qz" ]; then
     cat "$file" >> build/compiler/compiler.qz
   fi
