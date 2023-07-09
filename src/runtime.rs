893d83ebd5c1359a3e8822be915dfb6e83e256ff
myuon <ioi.joi.koi.loi@gmail.com>
2023-07-09 22:43:57 +0900

    feat: detailed error

--- src/runtime.rs
+++ src/runtime.rs
@@ -82,31 +82,31 @@ impl Runtime {
             let message = err.to_string();
             // regexp test (at offset %d) against message
             let Ok(re) = regex::Regex::new(r"\(at offset (\d+)\)") else {
-                return anyhow!("Original Error: {}", err);
+                return anyhow!("Original Error: {:?}", err);
             };
             let Some(cap) = re.captures(&message) else {
-                return anyhow!("Original Error: {}", err);
+                return anyhow!("Original Error: {:?}", err);
             };
             let Ok(offset) = cap[1].parse::<usize>() else {
-                return anyhow!("Original Error: {}", err);
+                return anyhow!("Original Error: {:?}", err);
             };
 
             let wasm = wat::parse_str(input).unwrap();
 
             let Ok(mut file) = std::fs::File::create("build/build.wat") else {
-                return anyhow!("Original Error: {}", err);
+                return anyhow!("Original Error: {:?}", err);
             };
             file.write_all(input.as_bytes()).unwrap();
 
             let Ok(mut file) = std::fs::File::create("build/error.wasm") else {
-                return anyhow!("Original Error: {}", err);
+                return anyhow!("Original Error: {:?}", err);
             };
             file.write_all(&wasm).unwrap();
 
             // offset in base-16
             let offset_hex = format!("{:x}", offset);
 
-            anyhow!("Error at: {}\n\nOriginal Error: {}", offset_hex, err)
+            anyhow!("Error at: {}\n\nOriginal Error: {:?}", offset_hex, err)
         })
     }
 }
