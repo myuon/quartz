803f1f49a4d24f34c7a2370f84eff010087c71b2
myuon <ioi.joi.koi.loi@gmail.com>
2023-07-23 16:27:52 +0900

    fix: remove semaphore, set timeout

--- quartz-vscode/server/src/server.ts
+++ quartz-vscode/server/src/server.ts
@@ -11,19 +11,11 @@ import { exec } from "child_process";
 import * as path from "path";
 import * as util from "util";
 
-let globalControllers: AbortController[] = [];
-
 const execAsync_ = util.promisify(exec);
 
 const execAsync = async (command: string) => {
-  if (globalControllers.length > 3) {
-    globalControllers[0].abort();
-    globalControllers = globalControllers.slice(1);
-  }
-
   const controller = new AbortController();
   const { signal } = controller;
-  globalControllers.push(controller);
 
   setTimeout(() => {
     try {
@@ -31,7 +23,7 @@ const execAsync = async (command: string) => {
     } catch (err) {
       console.log(`Aborted: ${err}`);
     }
-  }, 10000);
+  }, 30000);
 
   const child = await execAsync_(command, { signal });
 
